5a2bf8a37a15ccc10d790f00564157fbeffcf0fd
pavelzack2002@gmail.com <pavelzack2002@gmail.com>
2022-11-17 14:33:21 +1000

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _3._1_pr_isrpo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
