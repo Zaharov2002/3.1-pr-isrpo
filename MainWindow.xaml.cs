8e9d3f1858854757c46d5b41c48fc808e155ed04
pavelzack2002@gmail.com <pavelzack2002@gmail.com>
2022-11-17 14:35:16 +1000

    /

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,160 @@ namespace _3._1_pr_isrpo
         {
             InitializeComponent();
         }
+        int summ;
+        private void Button1(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox1.Text);
+            summ = s1 + 3;
+            TextBox1.Text = summ.ToString();
+        }
+
+        private void Button2(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox1.Text);
+            summ = s1 + 2;
+            TextBox1.Text = summ.ToString();
+        }
+
+        private void Button3(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox1.Text);
+            summ = s1 + 1;
+            TextBox1.Text = summ.ToString();
+        }
+
+        private void Button4(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox1.Text);
+            summ = s1 - 1;
+            TextBox1.Text = summ.ToString();
+        }
+
+        private void Button5(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox1.Text);
+            summ = s1 - 2;
+            TextBox1.Text = summ.ToString();
+        }
+
+        private void Button6(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox1.Text);
+            summ = s1 - 3;
+            TextBox1.Text = summ.ToString();
+        }
+
+        private void Button7(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox2.Text);
+            summ = s1 + 3;
+            TextBox2.Text = summ.ToString();
+        }
+
+        private void Button8(object sender, RoutedEventArgs e)
+        {
+            int cvn1;
+            cvn1 = Convert.ToInt32(TextBox2.Text);
+            summ = cvn1 + 2;
+            TextBox2.Text = summ.ToString();
+
+        }
+
+        private void Button9(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox2.Text);
+            summ = s1 + 1;
+            TextBox2.Text = summ.ToString();
+
+        }
+
+        private void Button10(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox2.Text);
+            summ = s1 - 1;
+            TextBox2.Text = summ.ToString();
+
+        }
+
+        private void Button11(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox2.Text);
+            summ = s1 - 2;
+            TextBox2.Text = summ.ToString();
+
+        }
+
+        private void Button12(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox2.Text);
+            summ = s1 - 3;
+            TextBox2.Text = summ.ToString();
+
+        }
+
+        private void Button13(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox3.Text);
+            summ = s1 + 3;
+            TextBox3.Text = summ.ToString();
+
+        }
+
+        private void Button14(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox3.Text);
+            summ = s1 + 2;
+            TextBox3.Text = summ.ToString();
+
+        }
+
+        private void Button15(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox3.Text);
+            summ = s1 + 1;
+            TextBox3.Text = summ.ToString();
+
+        }
+
+        private void Button16(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox3.Text);
+            summ = s1 - 1;
+            TextBox3.Text = summ.ToString();
+
+        }
+
+        private void Button17(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox3.Text);
+            summ = s1 - 2;
+            TextBox3.Text = summ.ToString();
+
+        }
+
+        private void Button18(object sender, RoutedEventArgs e)
+        {
+            int s1;
+            s1 = Convert.ToInt32(TextBox3.Text);
+            summ = s1 - 3;
+            TextBox3.Text = summ.ToString();
+        }
     }
 }
+
